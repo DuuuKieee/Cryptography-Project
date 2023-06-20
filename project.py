a17fc5b171cb34edd2b84f5ddbf661ff959681bf
DuuuKieee 1604 <tranthanhloisd@gmail.com>
2023-06-20 23:45:02 +0700

    qr and sign

--- project.py
+++ project.py
@@ -10,18 +10,18 @@ from reportlab_qrcode import QRCodeImage
 from benchmark_dilithium import benchmark_dilithium
 from reportlab.platypus import Paragraph, SimpleDocTemplate
 from PyPDF2 import PdfReader, PdfWriter
-
+import os
 
 
 CONNECTION_STRING = "mongodb+srv://DuuuKieee:899767147@loginserver.hqnkiia.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(CONNECTION_STRING)
 dbname = "CryptoProject"
 db = client[dbname]
-signature_collection = db["SignatureCollection"]
 file_collection = db["fs.files"]
 account = "test"
 def main():
     # Connect to MongoDB
+    
     global account 
     print("Account:")
     account = input()
@@ -38,9 +38,9 @@ def main():
         if(command == "/publish"):
             PublisherPermission()
         elif(command == "/verify"):
-            RecepientPermission(signature_collection)
+            RecepientPermission()
         elif(command=="/download"):
-            download_file(file_collection)
+            download_file()
         elif(command=="/benchmark"):
             # print("Số lần gọi thuật toán để đo hiệu suất:")
             # count = input()
@@ -53,9 +53,9 @@ def main():
         if(command =="/publish"):
             print("Permission denied")
         elif(command=="/verify"):
-            RecepientPermission(signature_collection)
+            RecepientPermission()
         elif(command=="/download"):
-            download_file(file_collection)
+            download_file()
         else: 
             print("Command not found for client!") 
     print("---------------------------")
@@ -65,8 +65,8 @@ def makeWatermark():
     watermarkName = "qr.pdf"
     doc = canvas.Canvas(watermarkName)
     qr = QRCodeImage(
-        size=25 * mm,
-        fill_color='blue',
+        size=30 * mm,
+        fill_color='black',
         back_color=(0, 0, 0, 0),
         border=4,
         version=2,
@@ -77,7 +77,7 @@ def makeWatermark():
     qr.add_data("Signed by: "+account+ "\n"+"Day/time: "+ dt_string)
     # qr.add_data(account+ "\n")
     # qr.make(fit=True)
-    qr.drawOn(doc, 30 * mm, 50 * mm)
+    qr.drawOn(doc, 20 * mm, 40 * mm)
     doc.save()
     return watermarkName
 
@@ -89,9 +89,15 @@ def makePdf(src, watermark):
         watermark_page = watermark_pdf.pages[0]
         output = PdfWriter()
 
-        pdf_page = input_pdf.pages[0]
-        pdf_page.merge_page(watermark_page)
-        output.add_page(pdf_page)
+        for i in range(len(input_pdf.pages)):
+            if(i==0):
+                pdf_page = input_pdf.pages[i]
+                pdf_page.merge_page(watermark_page)
+                output.add_page(pdf_page)
+            else:
+                pdf_page = input_pdf.pages[i]
+                output.add_page(pdf_page)
+                
 
         with open(merged, "wb") as merged_file:
             output.write(merged_file)
@@ -108,25 +114,19 @@ def PublisherPermission():
         with open(path, "rb") as file:
             pdf_file = file.read()
         sig = Dilithium3.sign(sk, pdf_file)
-
-        fs = gridfs.GridFS(signature_collection.database)
-        with open(path, "ab") as f:
-            f.write(sig)
-            
-        fs = gridfs.GridFS(signature_collection.database)
-        with open(path, "rb") as file:
-            file_id = fs.put(pdf_file, filename = file_name, publisher = account, publickey = pk )
         sig_hex = binascii.hexlify(sig).decode('utf-8')
         pkh_ex = binascii.hexlify(pk).decode('utf-8')
-        with open(path, "rb") as f:
-            f.seek(-3293, 2)  # Đặt con trỏ đọc tại vị trí cần đọc
-            last_bytes = f.read()  # Đọc 3,293 byte cuối
-        with open('signed.pdf', "wb") as f:
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y")
+
+        fs = gridfs.GridFS(file_collection.database)
+        with open(path, "ab") as f:
+            f.write(sig)
+        file_id = fs.put(pdf_file, filename = file_name, publisher = account,Date = dt_string, publickey = pkh_ex )
+        with open(file_name+"_signed.pdf", "wb") as f:
             f.write(pdf_file)
-        signature_collection.insert_one({
-        "signature": sig_hex,
-        "public_key": binascii.hexlify(pk).decode('utf-8')
-        })
+
+        
     
         print("Published!")
     except Exception as e: 
@@ -135,19 +135,22 @@ def PublisherPermission():
         PublisherPermission()
     #     # Upload signature to MongoDB
 
-def RecepientPermission(collection):
+def RecepientPermission():
     print("File path:")
     path = input()
     
     flag = 0
     try:
         for document in file_collection.find():
-            signature = binascii.unhexlify(document["signature"])
-            public_key = binascii.unhexlify(document["public_key"])
+            public_key = binascii.unhexlify(document["publickey"])
             with open(path, "rb") as file:
-                pdf_file = file.read()
-                file.seek(-3293, 2)  # Đặt con trỏ đọc tại vị trí cần đọc
-                signature = file.read()  # Đọc 3,293 byte cuối
+                file.seek(0, os.SEEK_END)
+                file_size = file.tell()
+                file.seek(0, os.SEEK_SET)
+                pdf_file = file.read(file_size - 3293)
+                file.seek(-3293, 2)  
+                signature = file.read()  
+            sig_hex = binascii.hexlify(signature).decode('utf-8')
             verify = Dilithium3.verify(public_key, pdf_file, signature)
             if(verify == True):
                 print(f"Verification result for signature {document['_id']}: {verify}")
@@ -158,15 +161,17 @@ def RecepientPermission(collection):
         if(flag != 0):
             print("false")
              
-    except:
-        print("File khong hop le")
-        RecepientPermission(collection)
+    except Exception as e: 
+        print(e)
+        print("File khong hop le2")
+        RecepientPermission(file_collection)
 
-def download_file(collection):
-    list_files(collection)
+def download_file():
+    list_files()
+    search(input())
     print("File name:")
     file_name = input()
-    fs = gridfs.GridFS(collection.database)
+    fs = gridfs.GridFS(file_collection.database)
     file = fs.find_one({"filename": file_name})
 
     if file:
@@ -175,14 +180,44 @@ def download_file(collection):
         print("Downloaded successfully!")
     else:
         print("File not found!")
-        download_file(collection)
+        download_file()
 
-def list_files(collection):
+def list_files():
     print("Available files:")
-    for i, document in enumerate(collection.find(), 1):
+    for i, document in enumerate(file_collection.find(), 1):
         file_name = document["filename"]
         file_date = document["uploadDate"]
         print(f"{i}: {file_name} (Uploaded on: {file_date})")
+
+def search(query):
+    pipeline = [
+        {
+            "$search": {
+                "index": "Searching",
+                "text": {
+                    "query": query,
+                    "path": {
+                        "wildcard": "*"
+                    }
+                    
+                }
+                
+            
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "filename": 1,
+                "Date": 1
+            }
+        }              
+]
+    
+    
+    results = file_collection.aggregate(pipeline)
+    for i, doc in enumerate(list(results), start=1):
+        print(f"{i}: {doc}")
 
 def bench_mark(path):
     count = 10
